fe701f6a5e5138cfb554822df9827a51b92bb815
ziepziep <hugo.eidmann@hotmail.com>
2021-04-30 17:32:52 +0200

    final version

--- heltal.cpp
+++ heltal.cpp
@@ -1,12 +1,14 @@
 #include <cstdlib>
-// Integer class 
 
+// Integer class 
 class Heltal{
 	public:
 		Heltal(int);
 		int get();
 		void set(int);
+		long long fib();
 	private:
+		long long _fib(int);
 		int val;
 	};
  
@@ -21,11 +23,24 @@ int Heltal::get(){
 void Heltal::set(int n){
 	val = n;
 	}
-	
+long long Heltal::fib(){
+	long long ret =  Heltal::_fib(val);
+	return ret;
+}
+long long Heltal::_fib(int n){
+	if(n<=1) {
+      return((long long) n);
+   }else {
+      return(Heltal::_fib(n-1)+Heltal::_fib(n-2));
+   }
+}
+
+
 
 extern "C"{
 	Heltal* Heltal_new(int n) {return new Heltal(n);}
 	int Heltal_get(Heltal* heltal) {return heltal->get();}
+	long long Heltal_fib(Heltal* heltal) {return heltal->fib();}
 	void Heltal_set(Heltal* heltal, int n) {heltal->set(n);}
 	void Heltal_delete(Heltal* heltal){
 		if (heltal){
--- heltal.py
+++ heltal.py
@@ -1,5 +1,11 @@
 """ Python interface to the C++ Integer class """
 import ctypes
+from time import perf_counter as pc
+from functools import cache
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+
 lib = ctypes.cdll.LoadLibrary('./libheltal.so')
 
 class Heltal(object):
@@ -7,6 +13,8 @@ class Heltal(object):
 		lib.Heltal_new.argtypes = [ctypes.c_int]
 		lib.Heltal_new.restype = ctypes.c_void_p
 		lib.Heltal_get.argtypes = [ctypes.c_void_p]
+		lib.Heltal_fib.argtypes = [ctypes.c_void_p]
+		lib.Heltal_fib.restype = ctypes.c_longlong # fib
 		lib.Heltal_get.restype = ctypes.c_int
 		lib.Heltal_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Heltal_delete.argtypes = [ctypes.c_void_p]
@@ -28,13 +36,57 @@ class Heltal(object):
 			return n
 		else:
 			return self.fib_py(n-1) + self.fib_py(n-2)
+	
+	def fib(self):
+		return lib.Heltal_fib(self.obj)
 
+def fib_py(n):
+	if n <= 1:
+		return n
+	return fib_py(n-1) + fib_py(n-2)
+def display_fib_comp():	
+	py_times = []
+	c_times = []
+	_range = range(30, 45)
+	
+	plt.figure(figsize=(10, 3))
+	plt.title("Comparing C++ and Python3 for fib(n)")
+	plt.ylabel("n")
+	plt.xlabel("Time (seconds)")
+	plt.yticks(_range)
+	plt.ylim(min(_range)-1, max(_range)+1, 1)
+	
+	for i in _range:
+		py_time, c_time = compare_fib(i)
+		py_times.append(py_time)
+		c_times.append(c_time)
+	
+	
+	plt.plot(py_times, _range, "-o", c="blue",label= "Python3")
+	plt.plot(c_times, _range, "-o", c="orange", label="c++")
+	plt.legend()
+
+	
+	
+	plt.savefig("fib_times_comparison.png")
+	plt.show()
+def compare_fib(n):
+	start = pc()
+	fib_py(n)
+	end = pc()
+	py_time = end-start
+	
+	start = pc()
+	h = Heltal(n)
+	h.fib()
+	end = pc()
+	c_time = end-start
+	return py_time, c_time
+def print_47th():
+	h = Heltal(47)
+	print(h.fib())
 def main():
-	h = Heltal(15)
-	print(h.fib_py())
+	pass
 
 if __name__ == '__main__':
 	main()
-
-
-
